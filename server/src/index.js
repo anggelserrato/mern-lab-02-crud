3efad2479da27465e0c694b4b81d9181b3e7fe08
Angel Serrato <serrato.anggel@gmail.com>
2026-03-02 08:32:03 -0500

    feat(server): refactor with routes and controllers structure

---
+++ server/src/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import statusRoutes from './routes/statusRoutes.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(statusRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({ message: err.message, status: 'error' });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not Found', status: 'error' });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port http://localhost:${process.env.PORT}`);
+});
