69573dc3a66ec0a23376f817da3c4dba8c5a56e1
Angel Serrato <serrato.anggel@gmail.com>
2026-03-04 08:46:20 -0500

    feat: implement Joi validation middleware and schemas - closes #3

--- server/src/routes/taskRoutes.js
+++ server/src/routes/taskRoutes.js
@@ -6,10 +6,19 @@ import {
   updateTask,
   deleteTask,
 } from '../controllers/taskController.js';
+import { validate } from '../middlewares/validate.js';
+import {
+  createTaskSchema,
+  updateTaskSchema,
+} from '../validators/taskValidator.js';
 
 const router = express.Router();
 
-router.route('/').get(getAllTasks).post(createTask);
-router.route('/:id').get(getTaskById).put(updateTask).delete(deleteTask);
+router.route('/').get(getAllTasks).post(validate(createTaskSchema), createTask);
+router
+  .route('/:id')
+  .get(getTaskById)
+  .put(validate(updateTaskSchema), updateTask)
+  .delete(deleteTask);
 
 export default router;
